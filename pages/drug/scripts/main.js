bb78930a98ec5b4b4fa761a114c34539e41970b6
missing00 <31206103+missing00@users.noreply.github.com>
2017-12-30 23:55:29 +0300

    add drug

---
+++ pages/drug/scripts/main.js
@@ -0,0 +1,52 @@
+
+'use strict';
+
+$( document ).ready( main )
+
+var	$letters	= $('.letters');
+
+var	objectLetters= {
+	'letters'	: {
+		'$d'		: $('.letters__letter-d'),
+		'$r'		: $('.letters__letter-r'),
+		'$u'		: $('.letters__letter-u'),
+		'$g'		: $('.letters__letter-g'),
+		'$cursor'	: $('.letters__letter-cursor'),
+	},
+	'functions'	: {
+		'show'		: function ( key, time ) {
+			console.log(key);
+		}
+	}
+};
+
+function main() {
+	var delay = 1;
+	addBlink( objectLetters.letters.$cursor );
+	for (var key in objectLetters.letters) {
+		delay++;
+		setDelay(objectLetters.letters, key, delay)
+	};
+	addBlink( objectLetters.letters );
+}
+
+
+
+function setDelay( array, key, delay ) {
+	setTimeout(function() {
+		if (key != '$g' && key != '$cursor') {
+			array[key].addClass('show');
+		}
+		else if (key == '$g') {
+			setTimeout(function() {
+				array[key].addClass('show');
+			}, 1000)
+		}
+		else if (key == '$cursor') {
+			array[key].addClass('blink');
+		}
+	}, delay*1000);
+}
+function addBlink( obj ) {
+	$(obj).addClass('blink');
+}
