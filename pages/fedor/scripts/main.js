48b1da2ff7df508fa16bcb5c6f7bea6713990224
Alexandr <AlMi@Mac-mini-Alexandr.local>
2018-02-18 15:41:09 +0300

    Fedor add Pictures page

--- pages/fedor/scripts/main.js
+++ pages/fedor/scripts/main.js
@@ -1,6 +1,10 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
 window.onload = function (ev) {
+
+
+
+
   var prev = $('.sliders__prev'),
       next = $('.sliders__next');
   $('.sliders__carousel').slick({
@@ -22,9 +26,11 @@ window.onload = function (ev) {
   $upBtn.on('click', function () {
     $(window).scrollTo( {top:'0px', left:'0px'} ,  500)
   });
+
   if ( window.pageYOffset > 0 ) {
     $header.addClass('scroll');
   }
+
   window.addEventListener('scroll', function(e) {
     if ( this.pageYOffset > 0 ) {
       $header.addClass('scroll');
